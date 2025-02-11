8748161c01decfc1ab5a0dd97496bb8c29628b42
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-12 00:16:21 +0530

    Remove unused mongoose import from user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import mongoose, { Aggregate } from "mongoose";
+// import mongoose, { Aggregate } from "mongoose";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import fs from "fs";
 import jwt from "jsonwebtoken";
